95c8f0a3f02d86d1b3d06f2330ad3640c376ccdb
Sören Tempel <tempel@uni-bremen.de>
2020-08-12 16:23:04 +0200

    clover: add branch.cpp

---
+++ branch.cpp
@@ -0,0 +1,14 @@
+#include <clover/clover.h>
+
+using namespace clover;
+
+Branch::Branch(unsigned id, std::shared_ptr<BitVector> bv)
+{
+	return;
+}
+
+bool
+Branch::getPath(unsigned id, std::vector<std::shared_ptr<BitVector>> &path)
+{
+	return false;
+}
